e52257608c171c8bbba51f996df9d0aa8fb1151a
Edd Hurst <edd@pragmatic-web.co.uk>
2016-12-04 22:55:34 +0000

    Add if function exists checks

--- url-breadcrumbs.php
+++ url-breadcrumbs.php
@@ -11,87 +11,97 @@
  * @version 1.1
  */
 
-/**
- * Retrieves hierarchical post information from current page URL.
- *
- * @return array	$breadcrumb_items	[x] => ( ['id'], ['title'], ['slug'], ['url'], ['type'] ).
- */
-function get_breadcrumbs() {
 
-	global $wp;
+if ( ! function_exists( 'get_breadcrumbs' ) ) {
 
-	// Split page URL into component parts by / character.
-	$breadcrumb_items = explode( '/', $wp->request );
+	/**
+	 * Retrieves hierarchical post information from current page URL.
+	 *
+	 * @return array	$breadcrumb_items	[x] => ( ['id'], ['title'], ['slug'], ['url'], ['type'] ).
+	 */
+	function get_breadcrumbs() {
 
-	$breadcrumb_url = esc_url( home_url( '/' ) );
+		global $wp;
 
-	// Loop through each breadcrumb item, to identify what content it links to.
-	foreach ( $breadcrumb_items as $key => $breadcrumb ) :
+		// Split page URL into component parts by / character.
+		$breadcrumb_items = explode( '/', $wp->request );
 
-		$breadcrumb_url = esc_url( $breadcrumb_url . $breadcrumb . '/' );
+		$breadcrumb_url = esc_url( home_url( '/' ) );
 
-		$breadcrumb_id = url_to_postid( $breadcrumb_url );
+		// Loop through each breadcrumb item, to identify what content it links to.
+		foreach ( $breadcrumb_items as $key => $breadcrumb ) :
 
-		// If breadcrumb_id has returned as non 0, it is a post / page / custom post.
-		if ( 0 !== $breadcrumb_id ) :
+			$breadcrumb_url = esc_url( $breadcrumb_url . $breadcrumb . '/' );
 
-			$breadcrumb_title = get_the_title( $breadcrumb_id );
-			$breadcrumb_type = get_post_type( $breadcrumb_id );
+			$breadcrumb_id = url_to_postid( $breadcrumb_url );
 
-		else :		// If breadcrumb_id is 0, page could be a taxonomy.
+			// If breadcrumb_id has returned as non 0, it is a post / page / custom post.
+			if ( 0 !== $breadcrumb_id ) :
 
-			$breadcrumb_id = get_cat_ID( $breadcrumb );
-			$breadcrumb_type = 'taxonomy';
-			$breadcrumb_title = get_cat_name( $breadcrumb_id );
+				$breadcrumb_title = get_the_title( $breadcrumb_id );
+				$breadcrumb_type = get_post_type( $breadcrumb_id );
 
-			// If breadcumb_id is still null, breadcrumb is likely a pseudo-page.
-			if ( 0 === $breadcrumb_id ) :
+			else :		// If breadcrumb_id is 0, page could be a taxonomy.
 
-				$breadcrumb_id = '';
-				$breadcrumb_title = $breadcrumb;
-				$breadcrumb_type = '';
+				$breadcrumb_id = get_cat_ID( $breadcrumb );
+				$breadcrumb_type = 'taxonomy';
+				$breadcrumb_title = get_cat_name( $breadcrumb_id );
+
+				// If breadcumb_id is still null, breadcrumb is likely a pseudo-page.
+				if ( 0 === $breadcrumb_id ) :
+
+					$breadcrumb_id = '';
+					$breadcrumb_title = $breadcrumb;
+					$breadcrumb_type = '';
+
+				endif;
 
 			endif;
 
-		endif;
+			$breadcrumb_items[ $key ] = array(
+				'id'	=> $breadcrumb_id,
+				'title'	=> $breadcrumb_title,
+				'slug'	=> $breadcrumb,
+				'url'	=> $breadcrumb_url,
+				'type'	=> $breadcrumb_type,
+			);
 
-		$breadcrumb_items[ $key ] = array(
-			'id'	=> $breadcrumb_id,
-			'title'	=> $breadcrumb_title,
-			'slug'	=> $breadcrumb,
-			'url'	=> $breadcrumb_url,
-			'type'	=> $breadcrumb_type,
-		);
+		endforeach;
 
-	endforeach;
+		return $breadcrumb_items;
 
-	return $breadcrumb_items;
+	}
 
 }
 
-/**
- * Generates breadcrumbs into user-friendly output.
- *
- * @param string $base_title     The title of the root element.
- * @param string $separator      The separator between elements.
- *
- * @return string	$breadcrumb_output		Output for breadcrumb trail, complete with links and separators.
- */
-function generate_breadcrumb_output( $base_title = 'Home', $separator = '&raquo;' ) {
 
-	// Add white space around separator.
-	$separator = ' ' . $separator . ' ';
+if ( ! function_exists( 'generate_breadcrumb_output' ) ) {
+	
+	/**
+	 * Generates breadcrumbs into user-friendly output.
+	 *
+	 * @param string $base_title     The title of the root element.
+	 * @param string $separator      The separator between elements.
+	 *
+	 * @return string	$breadcrumb_output		Output for breadcrumb trail, complete with links and separators.
+	 */
+	function generate_breadcrumb_output( $base_title = 'Home', $separator = '&raquo;' ) {
 
-	$breadcrumb_items = get_breadcrumbs();
+		// Add white space around separator.
+		$separator = ' ' . $separator . ' ';
 
-	$breadcrumbs_output = '<a href="' . home_url() . '" title="' . $base_title . '">' . $base_title . '</a>';
+		$breadcrumb_items = get_breadcrumbs();
 
-	foreach ( $breadcrumb_items as $key => $breadcrumb ) :
+		$breadcrumbs_output = '<a href="' . home_url() . '" title="' . $base_title . '">' . $base_title . '</a>';
 
-		$breadcrumbs_output .= $separator . '<a href="' . $breadcrumb['url'] . '" title="' . $breadcrumb['title'] . '">' . $breadcrumb['title'] . '</a>';
+		foreach ( $breadcrumb_items as $key => $breadcrumb ) :
 
-	endforeach;
+			$breadcrumbs_output .= $separator . '<a href="' . $breadcrumb['url'] . '" title="' . $breadcrumb['title'] . '">' . $breadcrumb['title'] . '</a>';
 
-	return $breadcrumbs_output;
+		endforeach;
+
+		return $breadcrumbs_output;
+
+	}
 
 }
